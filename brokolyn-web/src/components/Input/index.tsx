560b33b2823d78045a35236d79fb937881ae2e50
Douglas <douglas.lisboa@edu.unipar.br>
2020-07-23 20:01:18 -0300

    Implementado layout inicial de view de login.

---
+++ brokolyn-web/src/components/Input/index.tsx
@@ -0,0 +1,20 @@
+import React, { InputHTMLAttributes, ComponentType } from 'react';
+import { Container } from './styles';
+import { IconBaseProps } from 'react-icons';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
+  <>
+    <Container>
+      {Icon && <Icon size={20} />}
+      <input { ...rest } />
+    </Container>
+  </>
+)
+
+
+export default Input;
--- brokolyn-web/src/pages/SignIn/index.tsx
+++ brokolyn-web/src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo-brokolyn-barber.png';
 import { Container, Content, Background } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 const SignIn: React.FC = () => (
   <Container>
     <Content>
@@ -9,11 +11,11 @@ const SignIn: React.FC = () => (
 
       <form>
         <h1>Faça seu logon</h1>
-        <input type="text" placeholder="E-mail"/>
+        <Input name="email" icon={FiMail} placeholder="E-mail"/>
 
-        <input type="password" placeholder="Senha"/>
+        <Input name="password" icon={FiLock} placeholder="Senha"/>
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forget">
           Esqueci minha senha
